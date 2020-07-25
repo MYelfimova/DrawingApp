f5a87382e64fde22ded7954e70aed1552f9d0cb0
Maria Yelfimova <maria.yelfimova@gmail.com>
2020-07-25 18:07:10 +0300

    The items on the top are moving smoothly

---
+++ DrawingApp/EmojiCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiCollectionViewCell.swift
+//  DrawingApp
+//
+//  Created by Maria Yelfimova on 7/19/20.
+//  Copyright © 2020 Maria Yelfimova. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
